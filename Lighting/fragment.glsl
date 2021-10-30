d97419afeb05aa9c47c0b6389a45f9091a8c1132
Filmon97 <filmon67@hotmail.it>
2021-10-30 15:44:59 +0200

    Add specific matrix for normal vectors

--- Lighting/fragment.glsl
+++ Lighting/fragment.glsl
@@ -14,6 +14,6 @@ void main() {
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = diff * lightColor;
-	vec3 result = (ambient+diffuse) * objectColor;
+	vec3 result = (ambient + diffuse) * objectColor;
 	FragColor = vec4(result, 1.0);
 }
